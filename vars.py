b62dda9b0a15032e87539861d303d60b9e6ccf30
Imlapta <113951333+Imlapta@users.noreply.github.com>
2025-03-30 18:56:43 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,7 +5,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "3737117"))
 API_HASH = environ.get("API_HASH", "f466ca6ff400954d192ce0992ddf8b5d")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7948276603:AAGOOpHsQcPZ3ILpw5BX9GuwBbAGrvEGjX8")
 
   AUTH_USER = os.environ.get("AUTH_USERS", "1110590703").split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]  # Ensuring list of integers
